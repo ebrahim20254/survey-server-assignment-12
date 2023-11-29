4dc624c14c96dbe624a45771e82aae157f1dfc00
ebrahim20254 <ebrahimkhalil125800@gmail.com>
2023-11-29 23:36:12 +0600

    admin server user done

--- index.js
+++ index.js
@@ -34,12 +34,45 @@ async function run() {
 
     // user related api 
 
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post('/users', async(req, res) => {
       const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+      }
       const result = await userCollection.insertOne(user);
       res.send(result);
     })
 
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
+
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
+    // featured related api 
     app.get('/survey', async(req, res) => {
       const result = await surveyCollection.find().toArray();
       res.send(result);
